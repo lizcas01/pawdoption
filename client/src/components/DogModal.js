bae8604c795795895e848fd9f52269151b1747a8
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-05-25 17:11:05 -0600

    Deploy

--- client/src/components/DogModal.js
+++ client/src/components/DogModal.js
@@ -2,46 +2,50 @@ import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 const DogModal = ({ isOpen, onClose, dog }) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    const styles = {
-        card: {
-          display: 'flex',
-          alignItems: 'center',
-        },
-        image: {
-          height: "200px",
-          width: "300px",
-          borderRadius: "10px",
-          boxShadow: "0px 50px 70px rgba(0,0,0, 0.1)",
-          margin: "10px",
-        },
-      }
-   
+  const styles = {
+    card: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    image: {
+      height: '200px',
+      width: '300px',
+      borderRadius: '10px',
+      boxShadow: '0px 50px 70px rgba(0,0,0, 0.1)',
+      margin: '10px',
+    },
+  };
 
-    return (
-        <Modal show={isOpen} onHide={onClose} centered>
-            <Modal.Header>
-                <Modal.Title className="w-100 text-center">{dog.name}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="text-center">
-                <img src={dog.picture} alt={dog.name} style={styles.image}/>
-                <p>{dog.gender} {dog.breed}</p>
-                <p>Age: {dog.age}</p>
-                <p>Size: {dog.size}</p>
-                <p>{dog.description}</p>
-                <Button variant="outline-primary" className='w-75' onClick={onClose}>
-                    Apply to Adopt {dog.name}!
-                </Button>
-            </Modal.Body>
-            <Modal.Footer>                
-                <Button variant="secondary" onClick={onClose}>
-                    Close
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  const handleApply = () => {
+    window.location.href = '/apply';
+  };
+
+  return (
+    <Modal show={isOpen} onHide={onClose} centered>
+      <Modal.Header>
+        <Modal.Title className="w-100 text-center">{dog.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="text-center">
+        <img src={dog.picture} alt={dog.name} style={styles.image} />
+        <p>
+          {dog.gender} {dog.breed}
+        </p>
+        <p>Age: {dog.age}</p>
+        <p>Size: {dog.size}</p>
+        <p>{dog.description}</p>
+        <Button variant="outline-primary" className="w-75" onClick={handleApply}>
+          Apply to Adopt {dog.name}!
+        </Button>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default DogModal;
-
