f22c41211c7d99094249b6b9e85263fa2dc9f1a5
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-05-25 17:08:00 -0600

    Deploy

--- server/server.js
+++ server/server.js
@@ -19,11 +19,9 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
